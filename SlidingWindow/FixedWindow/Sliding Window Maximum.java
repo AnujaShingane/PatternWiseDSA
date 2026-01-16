0de64c82883a26c4b3cfe27ea73b4c712e0e983b
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-01-16 14:31:13 +0530

    Update Sliding Window Maximum.java

--- SlidingWindow/FixedWindow/Sliding Window Maximum.java
+++ SlidingWindow/FixedWindow/Sliding Window Maximum.java
@@ -20,86 +20,28 @@ Window position                Max
  1  3  -1  -3  5 [3  6  7]      7
 */
 
-class Pair{
-    int ind;
-    int val;
-
-    Pair(int ind,int val){
-        this.ind=ind;
-        this.val = val;
-    }
-}
-
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        int n = nums.length;
-        int[] arr = new int[n-k+1];
-
-        Deque<Pair> q = new ArrayDeque<>();
-        int ind=0;
+    public ArrayList<Integer> maxOfSubarrays(int[] arr, int k) {
+        int n = arr.length;
+        ArrayList<Integer> list = new ArrayList<>();
+        Deque<Integer> dq = new ArrayDeque<>();
+        
         for(int i = 0 ; i < n ; i++){
-
-            while(!q.isEmpty() && q.peekLast().val<=nums[i]){
-                q.pollLast();
+            while(!dq.isEmpty() && arr[dq.peekLast()]<arr[i]){
+                dq.pollLast();
             }
-
-            if(q.isEmpty() || q.peekLast().val>nums[i]){
-                q.add(new Pair(i,nums[i]));
+            while(!dq.isEmpty() && dq.peekFirst()<i-k+1){
+                dq.pollFirst();
             }
-
-            if(!q.isEmpty() && q.peekFirst().ind <= i-k){
-                q.pollFirst();
-            }
-
+            
+            dq.addLast(i);
+            
+            //if in range start the process
             if(i>=k-1){
-                arr[ind++]=q.peekFirst().val;
+                list.add(arr[dq.peekFirst()]);
             }
         }
         
-        return arr;
+        return list;
     }
-}
-
-
-// using pq
-
-import java.util.*;
-
-class Pair {
-    int idx;
-    int val;
-
-    Pair(int idx, int val) {
-        this.idx = idx;
-        this.val = val;
-    }
-}
-
-class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        int n = nums.length;
-        int[] ans = new int[n - k + 1];
-        int pos = 0;
-
-        // max heap
-        PriorityQueue<Pair> pq = new PriorityQueue<>(
-            (a, b) -> b.val - a.val
-        );
-
-        for (int i = 0; i < n; i++) {
-            pq.add(new Pair(i, nums[i]));
-
-            // remove elements that are outside the window
-            while (pq.peek().idx <= i - k) {
-                pq.poll();
-            }
-
-            // when window is ready
-            if (i >= k - 1) {
-                ans[pos++] = pq.peek().val;
-            }
-        }
-
-        return ans;
-    }
-}
+} 
