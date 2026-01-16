cb311ba7040f97709fb6c38bdfcd68c57fc07a32
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-01-16 21:36:11 +0530

    Update Minimum size subarray sum ≥ K.java

--- SlidingWindow/VariableWindow/Minimum size subarray sum ≥ K.java
+++ SlidingWindow/VariableWindow/Minimum size subarray sum ≥ K.java
@@ -21,19 +21,22 @@ class Solution {
     public int minSubArrayLen(int target, int[] nums) {
         int n = nums.length;
         int l = 0;
+        int r = 0;
         int sum = 0;
-        int minLen = Integer.MAX_VALUE;
+        int minlen = Integer.MAX_VALUE;
 
-        for (int r = 0; r < n; r++) {
-            sum += nums[r];
+        while(r<n){
+            sum+=nums[r];
 
-            while (sum >= target) {
-                minLen = Math.min(minLen, r - l + 1);
-                sum -= nums[l];
+            while(sum>=target){
+                minlen=Math.min(minlen,r-l+1);
+                sum-=nums[l];
                 l++;
             }
+            r++;
         }
-
-        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+        
+        if(minlen==Integer.MAX_VALUE)return 0;
+        else return minlen;
     }
 }
