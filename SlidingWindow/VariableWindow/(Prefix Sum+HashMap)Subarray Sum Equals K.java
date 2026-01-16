f986a2fb33b1ac9d15daca425f41195a85e0ad03
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-01-16 13:36:52 +0530

    Update and rename Subarray Sum Equals K.java to (Prefix Sum+HashMap)Subarray Sum Equals K.java

--- SlidingWindow/VariableWindow/(Prefix Sum+HashMap)Subarray Sum Equals K.java
+++ SlidingWindow/VariableWindow/(Prefix Sum+HashMap)Subarray Sum Equals K.java
@@ -14,17 +14,18 @@ Output: 2
 
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int n = nums.length;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int sum = 0;
-        map.put(0,1);
-        int cnt = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
 
-        for(int i = 0 ; i < n ; i++){
-            sum += nums[i];
-            if(map.containsKey(sum-k))cnt+=map.get(sum-k);
-            map.put(sum,map.getOrDefault(sum,0)+1);
+        int sum = 0, count = 0;
+
+        for (int num : nums) {
+            sum += num;
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
-        return cnt;
+        return count;
     }
 }
