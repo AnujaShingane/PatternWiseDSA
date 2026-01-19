6a8026a64d70d733ba7ad9a475e9e12b7a89ab33
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-01-19 11:59:56 +0530

    Update Majority Element.java

--- FrequencyCounting/Basic Frequency (Foundation)/Majority Element.java
+++ FrequencyCounting/Basic Frequency (Foundation)/Majority Element.java
@@ -1,1 +1,37 @@
+/*
+Given an array nums of size n, return the majority element.
 
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+*/
+
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        int cnt = 0;
+        int num = -1;
+
+        for(int i = 0 ; i < n ;i++){
+            if(cnt==0){
+                cnt++;
+                num=nums[i];
+            }else{
+                if(nums[i]==num)cnt++;
+                else cnt--;
+            }
+        }
+
+        return num;
+    }
+}
