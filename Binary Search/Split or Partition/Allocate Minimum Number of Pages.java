c7dca8a0d0c83ef11955a95f9d1328e203e8b4c0
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-02-03 20:58:56 +0530

    Update Allocate Minimum Number of Pages.java

--- Binary Search/Split or Partition/Allocate Minimum Number of Pages.java
+++ Binary Search/Split or Partition/Allocate Minimum Number of Pages.java
@@ -1,1 +1,71 @@
+/*
+Given an array arr[] of integers, where each element arr[i] represents the number of pages in the i-th book. You also have an integer k representing the number of students. The task is to allocate books to each student such that:
 
+Each student receives atleast one book.
+Each student is assigned a contiguous sequence of books.
+No book is assigned to more than one student.
+The objective is to minimize the maximum number of pages assigned to any student. In other words, out of all possible allocations, find the arrangement where the student who receives the most pages still has the smallest possible maximum.
+
+Note: If it is not possible to allocate books to all students, return -1.
+
+Examples:
+
+Input: arr[] = [12, 34, 67, 90], k = 2
+Output: 113
+Explanation: Allocation can be done in following ways:
+=> [12] and [34, 67, 90] Maximum Pages = 191
+=> [12, 34] and [67, 90] Maximum Pages = 157
+=> [12, 34, 67] and [90] Maximum Pages = 113.
+The third combination has the minimum pages assigned to a student which is 113.
+Input: arr[] = [15, 17, 20], k = 5
+Output: -1
+Explanation: Since there are more students than total books, it's impossible to allocate a book to each student.
+*/
+
+
+class Solution {
+    public int findPages(int[] arr, int k) {
+        int n = arr.length;
+        if(k>n)return -1;
+        
+        int low = Integer.MIN_VALUE; // max in array
+        int high = 0; // sum of array
+        
+        for(int num : arr){
+            low = Math.max(low,num);
+            high+=num;
+        }
+        
+        int ans = -1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            int noStudents = students(arr,mid);
+            
+            if(noStudents<=k){
+                ans = mid;
+                high=mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        
+        return ans;
+    }
+    
+    public static int students(int[] arr, int maxPages){
+        int n = arr.length;
+        int student = 1;
+        int sum = 0;
+        
+        for(int i = 0 ; i < n ; i++){
+            if(sum+arr[i]>maxPages){
+                student++;
+                sum=arr[i];
+            }else{
+                sum+=arr[i];
+            }
+        }
+        
+        return student;
+    }
+}
