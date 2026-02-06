e7246cc6d2221d016539640e3e5b9a4b64b7a076
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-02-06 20:26:57 +0530

    Update Find if Path Exists in Graph.java

--- BFS & DFS/Graph BFS DFS/Find if Path Exists in Graph.java
+++ BFS & DFS/Graph BFS DFS/Find if Path Exists in Graph.java
@@ -1,1 +1,59 @@
+/*
+There is a bi-directional graph with n vertices, where each vertex is labeled from 0 to n - 1 (inclusive). The edges in the graph are represented as a 2D integer array edges, where each edges[i] = [ui, vi] denotes a bi-directional edge between vertex ui and vertex vi. Every vertex pair is connected by at most one edge, and no vertex has an edge to itself.
 
+You want to determine if there is a valid path that exists from vertex source to vertex destination.
+
+Given edges and the integers n, source, and destination, return true if there is a valid path from source to destination, or false otherwise.
+
+Example 1:
+
+
+Input: n = 3, edges = [[0,1],[1,2],[2,0]], source = 0, destination = 2
+Output: true
+Explanation: There are two paths from vertex 0 to vertex 2:
+- 0 → 1 → 2
+- 0 → 2
+Example 2:
+
+
+Input: n = 6, edges = [[0,1],[0,2],[3,5],[5,4],[4,3]], source = 0, destination = 5
+Output: false
+Explanation: There is no path from vertex 0 to vertex 5.
+*/
+
+
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0 ; i < n ; i++){
+            adj.add(new ArrayList<>());
+        }
+        adj = adjList(n,edges,adj);
+
+        int[] vis = new int[n];
+
+        dfs(source,vis,adj);
+
+        if(vis[destination]==0)return false;
+        return true;
+    }
+
+    public void dfs(int node,int[] vis, ArrayList<ArrayList<Integer>> adj) {
+        vis[node]=1;
+
+        for(int ele : adj.get(node)){
+            if(vis[ele]==0){
+                dfs(ele,vis,adj);
+            }
+        }
+    }
+
+    public ArrayList<ArrayList<Integer>> adjList(int n, int[][] edges,ArrayList<ArrayList<Integer>> adj) {
+        for(int[] arr : edges){
+            adj.get(arr[0]).add(arr[1]);
+            adj.get(arr[1]).add(arr[0]);
+        }
+
+        return adj;
+    }
+}
