be729ea9402c966b6b516689dbb60f52cb6ebd2d
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-02-09 19:51:32 +0530

    Add max heap solution for kth smallest element
    Implemented a max heap solution to find the kth smallest element in a sorted matrix.

--- Tier-3/Heap or Priority Queue/kth or top k elements/Kth Smallest Element in a Sorted Matrix.java
+++ Tier-3/Heap or Priority Queue/kth or top k elements/Kth Smallest Element in a Sorted Matrix.java
@@ -1,1 +1,21 @@
+Binary Search would be better here 
 
+
+Max heap soln :
+
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int[] arr : matrix){
+            for(int ele : arr){
+                pq.add(ele);
+                if(pq.size()>k){
+                    pq.poll();
+                }
+            }
+        }
+
+        return pq.peek();
+    }
+}
