b292738718b023b19d8bdcb3b0702cbeb82aa20d
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-02-25 20:25:54 +0530

    Update and rename Kth Smallest element in BST.java to Kth Smallest Largest element in BST.java

--- Tree/BST/Kth Smallest Largest element in BST.java
+++ Tree/BST/Kth Smallest Largest element in BST.java
@@ -43,3 +43,30 @@ class Solution {
         inorder(root.right,k);
     }
 }
+
+
+//largest
+
+class Solution {
+    int result = -1;
+    int cnt = 0;
+
+    public int kthLargest(Node root, int k) {
+        reverseInorder(root, k);
+        return result;
+    }
+
+    public void reverseInorder(Node root, int k) {
+        if (root == null || cnt >= k) return;
+
+        reverseInorder(root.right, k);
+
+        cnt++;
+        if (cnt == k) {
+            result = root.data;
+            return;
+        }
+
+        reverseInorder(root.left, k);
+    }
+}
