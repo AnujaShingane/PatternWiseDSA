4a94f45b72489e36df9013d70406282e141ae927
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-02-25 05:34:50 +0530

    Create Ceil in BST.java

---
+++ Tree/BST/Ceil in BST.java
@@ -0,0 +1,32 @@
+TC -> O(log N)
+SC -> O(N)
+
+/* class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+} */
+
+class Solution {
+    int findCeil(Node root, int x) {
+        int ceil = -1;
+        
+        while(root!=null){
+            if(root.data == x){
+                return x;
+            }
+            else if(root.data>x){
+                ceil=root.data;
+                root=root.left;
+            }else{
+                root=root.right;
+            }
+        }
+        
+        return ceil;
+    }
+}
