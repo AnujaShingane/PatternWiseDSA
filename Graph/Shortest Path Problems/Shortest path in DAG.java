64f21fabe13ae407fe85719fc8839e9b7974cf89
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-19 16:53:59 +0530

    Refactor shortest path algorithm in DAG

--- Graph/Shortest Path Problems/Shortest path in DAG.java
+++ Graph/Shortest Path Problems/Shortest path in DAG.java
@@ -3,73 +3,82 @@ class Pair{
     int wt;
     
     Pair(int node,int wt){
-        this.node = node;
+        this.node=node;
         this.wt=wt;
     }
 }
 
 class Solution {
     public int[] shortestPath(int V, int E, int[][] edges) {
-        int n1 = edges.length;
-        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-        for(int i = 0 ; i < V ; i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int i = 0 ; i < n1 ; i++){
-            int s = edges[i][0];
-            int e = edges[i][1];
-            int wt = edges[i][2];
-            adj.get(s).add(new Pair(e,wt));
-        }
-        
-        Stack<Integer> st = new Stack<>();
-        boolean[] vis = new boolean[V];
-        for(int i = 0 ; i < V ; i++){
-            if(!vis[i]){
-                dfs(i,st,vis,adj);
-            }
-        }
-        
+        ArrayList<ArrayList<Pair>> adj = adjList(V,E,edges);
+        Stack<Integer> st = topoSort(adj);
         int[] dist = new int[V];
-        Arrays.fill(dist,(int)(1e9));
-        dist[0]=0;
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[0] = 0;
         
+        //relax edges and store distance in dist arr till st does not become empty
         while(!st.isEmpty()){
-            int node = st.pop();
-            int dis = dist[node];
+            int n = st.pop();
+            int d = dist[n];
+            if(d==Integer.MAX_VALUE)continue;
             
-            for(Pair p : adj.get(node)){
-                int n = p.node;
-                int d = p.wt;
+            for(Pair p : adj.get(n)){
+                int ele = p.node;
+                int dis = p.wt;
                 
-                int finalDist = dis + d;
-                if(finalDist < dist[n]){
-                    dist[n] = finalDist;
-                }
+                int newDist = d+dis;
+                dist[ele] = Math.min(dist[ele],newDist);
             }
         }
         
-        
-        for(int i = 0 ; i < dist.length ; i++){
-            if(dist[i]==(int)(1e9)){
-                dist[i]=-1;
+        for(int i = 0; i < V; i++){
+            if(dist[i] == Integer.MAX_VALUE){
+                dist[i] = -1;
             }
         }
         
         return dist;
     }
     
-    public void dfs(int node,Stack<Integer> st,boolean[] vis,ArrayList<ArrayList<Pair>> adj){
-        vis[node]=true;
+    public Stack<Integer> topoSort(ArrayList<ArrayList<Pair>> adj){
+        Stack<Integer> st = new Stack<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        boolean[] vis = new boolean[adj.size()];
+        int n = adj.size();
         
-        for(Pair elePair : adj.get(node)){
-            int ele = elePair.node;
-            
+        for(int i = 0 ; i < n ; i++){
+            if(!vis[i]){
+                dfs(i,adj,st,vis);
+            }
+        }
+        
+        return st;
+    }
+    
+    public void dfs(int node,ArrayList<ArrayList<Pair>> adj,Stack<Integer> st,
+                    boolean[] vis){
+        vis[node] = true;
+        
+        for(Pair p : adj.get(node)){
+            int ele = p.node;
             if(!vis[ele]){
-                dfs(ele,st,vis,adj);
+                dfs(ele,adj,st,vis);
             }
         }
         
         st.push(node);
+    }
+    
+    public ArrayList<ArrayList<Pair>> adjList(int V, int E, int[][] edges) {
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+        for(int i = 0 ; i < V ; i++){
+            adj.add(new ArrayList<>());
+        }
+        
+        for(int i = 0 ; i < E ; i++){
+            adj.get(edges[i][0]).add(new Pair(edges[i][1],edges[i][2]));
+        }
+        
+        return adj;
     }
 }
