1b1936b7521396c5169d9259cc745a5f9ab2113b
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-23 01:01:30 +0530

    Update Basic DFS.java

--- Graph/Basic DFS.java
+++ Graph/Basic DFS.java
@@ -1,1 +1,10 @@
+public void dfs(int node, boolean[] vis, List<List<Integer>> adj) {
+    vis[node] = true;
+    System.out.print(node + " "); // process node
 
+    for (int nei : adj.get(node)) {
+        if (!vis[nei]) {
+            dfs(nei, vis, adj);
+        }
+    }
+}
