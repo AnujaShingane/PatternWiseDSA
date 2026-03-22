7e11b93a6db1802f19060614a850e3fd9113b201
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-23 00:41:06 +0530

    Create Topological Sort DFS Based.java

---
+++ Graph/Topological Sort DFS Based.java
@@ -0,0 +1,9 @@
+public void topoDFS(int node, boolean[] vis, Stack<Integer> st, List<List<Integer>> adj) {
+    vis[node] = true;
+
+    for (int nei : adj.get(node)) {
+        if (!vis[nei]) topoDFS(nei, vis, st, adj);
+    }
+
+    st.push(node);
+}
