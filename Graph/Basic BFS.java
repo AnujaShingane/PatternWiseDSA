631290773d903d0bef3ca5203fa2510f82f1e58e
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-23 01:00:27 +0530

    Update Basic BFS.java

--- Graph/Basic BFS.java
+++ Graph/Basic BFS.java
@@ -1,1 +1,19 @@
+public void bfs(int start, List<List<Integer>> adj, int n) {
+    boolean[] vis = new boolean[n];
+    Queue<Integer> q = new LinkedList<>();
 
+    q.offer(start);
+    vis[start] = true;
+
+    while (!q.isEmpty()) {
+        int node = q.poll();
+        System.out.print(node + " "); // process node
+
+        for (int nei : adj.get(node)) {
+            if (!vis[nei]) {
+                vis[nei] = true;
+                q.offer(nei);
+            }
+        }
+    }
+}
