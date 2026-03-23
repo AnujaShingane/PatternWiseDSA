8d32ac6f77854f8e2c5fc264fcc5d3b4f0786da8
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-23 12:21:34 +0530

    Update Shortest Path Weighted Graph Bellman-Ford (Negative Edges).java

--- Graph/Shortest Path Weighted Graph Bellman-Ford (Negative Edges).java
+++ Graph/Shortest Path Weighted Graph Bellman-Ford (Negative Edges).java
@@ -1,3 +1,11 @@
+Bellman (weighted + positive/negative)
+    Use : dist arr
+
+        1. start with all 1e9 valued dist arr with dist[src] = 0
+        2. Do N-1 itertions and relax each edges -> (each edge will relax n-1 times then)
+        3. In Nth iteration agin check relaxation condition if true -> negative cycle exists
+        4. ** dist[u] != (int)(1e9) && dist[u] + wt < dist[v] -> dist[v] = dist[u]+wt ** -> Condition for relaxation
+
 public int[] bellmanFord(int n, int[][] edges, int src) {
     int[] dist = new int[n];
     Arrays.fill(dist, (int)1e9);
