9d96a5badcf466dfba685696056fd91c892d8564
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-23 13:13:53 +0530

    Update Cycle Detection Undirected Graph (BFS).java

--- Graph/Cycle Detection Undirected Graph (BFS).java
+++ Graph/Cycle Detection Undirected Graph (BFS).java
@@ -1,3 +1,10 @@
+Use : Queue + vis
+    imp -> the nei node to which we're approaching if it is already vis and also not a parent 
+           i.e the node that we just cam from then there exists a cycle
+
+    1. while !q.isEmpty -> travel to each nei if not vis offer it to queue while marking vis true
+    2. if vis and !parent -> return true
+
 public boolean bfsCycle(int src, List<List<Integer>> adj, boolean[] vis) {
     Queue<int[]> q = new LinkedList<>();
     q.offer(new int[]{src, -1});
