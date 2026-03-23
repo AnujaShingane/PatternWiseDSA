e0de293cc1fcb1543bde75406cc5d0cb30d4502e
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-23 13:01:23 +0530

    Update Cycle Detection Directed Graph (DFS + Rec Stack).java

--- Graph/Cycle Detection Directed Graph (DFS + Rec Stack).java
+++ Graph/Cycle Detection Directed Graph (DFS + Rec Stack).java
@@ -1,3 +1,9 @@
+Use : vis + pathVis
+    1. dfs overall
+    2. mark vis pathVis of node true 
+    3. for each nei of node if not vis dfs if vis and pathVis return true
+    4. while backtracking mark pathVis as false while returning
+    
 public boolean dfsDirected(int node, boolean[] vis, boolean[] pathVis, List<List<Integer>> adj) {
     vis[node] = true;
     pathVis[node] = true;
