a0e909249e75bc3a5fc7bd570049633dbab6db98
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-24 22:13:24 +0530

    Update Partition Equal Subset Sum.java

--- DP series/Partition Equal Subset Sum.java
+++ DP series/Partition Equal Subset Sum.java
@@ -1,1 +1,13 @@
-
+public static boolean helper(int idx, int[] arr, int target){
+    if(target == 0) return true;
+    if(idx == 0) return arr[0] == target;
+    
+    // take
+    boolean take = false;
+    if(arr[idx] <= target) take = helper(idx - 1, arr, target - arr[idx]);
+    
+    // not-take
+    boolean notTake = helper(idx - 1, arr, target);
+    
+    return take || notTake;
+}
