c2039085184fd16668e12ba0784fab212298ca31
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-31 21:55:13 +0530

    Update Maximum Size Subarray Sum Equals k.java

--- Prefix Sum/The Hash Map Powerhouses/Maximum Size Subarray Sum Equals k.java
+++ Prefix Sum/The Hash Map Powerhouses/Maximum Size Subarray Sum Equals k.java
@@ -1,31 +1,31 @@
-import java.util.*;
-
-public class Solution {
-    public static int longestSubarray(int[] arr, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+class Solution {
+    public int longestSubarray(int[] arr, int k) {
+        int n = arr.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
         int sum = 0;
-        int maxLen = 0;
-
-        for (int i = 0; i < arr.length; i++) {
+        int len = 0;
+        int maxlen = 0;
+        
+        for(int i = 0 ; i < n ; i++){
             sum += arr[i];
-
-            // Case 1: sum itself equals k
-            if (sum == k) {
-                maxLen = i + 1;
+            
+            //case1 : if equals k
+            if(sum==k){
+                maxlen = i+1;
+            }
+            
+            //case2 : if not equals k but contains sum-k
+            if(map.containsKey(sum-k)){
+                len = i-map.get(sum-k);
+                maxlen = Math.max(len,maxlen);
             }
 
-            // Case 2: (sum - k) seen before
-            if (map.containsKey(sum - k)) {
-                int len = i - map.get(sum - k);
-                maxLen = Math.max(maxLen, len);
-            }
-
-            // Store prefix sum only first time
-            if (!map.containsKey(sum)) {
-                map.put(sum, i);
+            //add sum and index to map
+            if(!map.containsKey(sum)){
+                map.put(sum,i);
             }
         }
-
-        return maxLen;
+        
+        return maxlen;
     }
 }
