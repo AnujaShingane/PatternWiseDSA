22fe1bd987f2ecae97d7922275f82e3616da26f6
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-31 14:50:33 +0530

    Update Maximum Size Subarray Sum Equals k.java

--- Prefix Sum/The Hash Map Powerhouses/Maximum Size Subarray Sum Equals k.java
+++ Prefix Sum/The Hash Map Powerhouses/Maximum Size Subarray Sum Equals k.java
@@ -1,1 +1,31 @@
+import java.util.*;
 
+public class Solution {
+    public static int longestSubarray(int[] arr, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+            // Case 1: sum itself equals k
+            if (sum == k) {
+                maxLen = i + 1;
+            }
+
+            // Case 2: (sum - k) seen before
+            if (map.containsKey(sum - k)) {
+                int len = i - map.get(sum - k);
+                maxLen = Math.max(maxLen, len);
+            }
+
+            // Store prefix sum only first time
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
+            }
+        }
+
+        return maxLen;
+    }
+}
