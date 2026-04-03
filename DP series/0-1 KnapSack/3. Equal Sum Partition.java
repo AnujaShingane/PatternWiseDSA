8a5b08aa5f106ec2578d096958859061eed75f54
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-04-03 11:44:52 +0530

    Update 3. Equal Sum Partition.java

--- DP series/0-1 KnapSack/3. Equal Sum Partition.java
+++ DP series/0-1 KnapSack/3. Equal Sum Partition.java
@@ -33,70 +33,61 @@ class Solution {
 
 Memo -->
 class Solution {
-    Boolean[][] dp;
-
     public boolean canPartition(int[] nums) {
         int n = nums.length;
         int sum = 0;
+        for(int ele : nums)sum+=ele;
+        if(sum%2!=0)return false;
+        int target = sum/2;
+        Boolean[][] dp = new Boolean[n][sum+1];
 
-        for(int num : nums) sum += num;
-
-        if(sum % 2 != 0) return false;
-
-        int target = sum / 2;
-        dp = new Boolean[n][target + 1];
-
-        return f(n - 1, target, nums);
+        return func(n-1,nums,target,dp);
     }
 
-    boolean f(int i, int target, int[] arr){
-        if(target == 0) return true;
-        if(i == 0) return arr[0] == target;
+    public boolean func(int ind,int[] nums,int target,Boolean[][] dp){
+        if(target==0)return true;
+        if(ind==0){
+            return nums[0] == target;
+        }
 
-        if(dp[i][target] != null) return dp[i][target];
+        if(dp[ind][target]!=null)return dp[ind][target];
 
-        boolean notTake = f(i - 1, target, arr);
-
+        boolean nottake = func(ind-1,nums,target,dp);
         boolean take = false;
-        if(arr[i] <= target)
-            take = f(i - 1, target - arr[i], arr);
+        if(nums[ind]<= target)take = func(ind-1,nums,target-nums[ind],dp);
 
-        return dp[i][target] = take || notTake;
+        return dp[ind][target] = take || nottake;
     }
 }
 
 
 Tabu -->
 class Solution {
-    public boolean canPartition(int[] arr) {
-        int n = arr.length;
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
         int sum = 0;
+        for(int ele : nums)sum+=ele;
+        if(sum%2!=0)return false;
+        int target = sum/2;
+        boolean[][] dp = new boolean[n][sum+1];
 
-        for(int num : arr) sum += num;
+        for(int i = 0 ; i < n ; i++){
+            dp[i][0] = true;
+        }
 
-        if(sum % 2 != 0) return false;
+        if(target>=nums[0])dp[0][nums[0]] = true;
 
-        int k = sum / 2;
-
-        boolean[][] dp = new boolean[n][k + 1];
-
-        for(int i = 0; i < n; i++) dp[i][0] = true;
-
-        if(arr[0] <= k) dp[0][arr[0]] = true;
-
-        for(int i = 1; i < n; i++){
-            for(int t = 1; t <= k; t++){
-                boolean notTake = dp[i - 1][t];
-
+        for(int ind = 1 ; ind < n ; ind++){
+            for(int t = 0 ; t <= sum ; t++){
+                boolean nottake = dp[ind-1][t];
                 boolean take = false;
-                if(arr[i] <= t)
-                    take = dp[i - 1][t - arr[i]];
+                if(nums[ind]<= t)take = dp[ind-1][t-nums[ind]];
 
-                dp[i][t] = take || notTake;
+                dp[ind][t] = take || nottake;
             }
         }
 
-        return dp[n - 1][k];
+        return dp[n-1][target];
     }
 }
 
