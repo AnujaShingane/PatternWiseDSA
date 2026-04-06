482ec13e395aeb1e20821821ae836635064f26de
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-04-06 23:32:04 +0530

    Update Longest Palindromic Subsequence.java

--- DP series/Dp on Strings/Longest Palindromic Subsequence.java
+++ DP series/Dp on Strings/Longest Palindromic Subsequence.java
@@ -1,1 +1,34 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        String s1 = new String(s);
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
+        String s2 = sb.toString();
+        return lcs(s1,s2);
+    }
 
+    public static int lcs(String s1, String s2){
+        StringBuilder sb = new StringBuilder();
+        int n1 = s1.length();
+        int n2 = s2.length();
+        int[][] dp = new int[n1+1][n2+1];
+        for(int[] arr : dp){
+            Arrays.fill(arr,-1);
+        }
+
+        return func(n1-1,n2-1,s1,s2,dp);
+    }
+
+    public static int func(int ind1,int ind2,String s1,String s2,int[][] dp){
+        if(ind1<0 || ind2<0)return 0;
+
+        if(dp[ind1][ind2]!=-1)return dp[ind1][ind2];
+
+        if(s1.charAt(ind1)==s2.charAt(ind2)){
+            return dp[ind1][ind2] = 1 + func(ind1-1,ind2-1,s1,s2,dp);
+        }else{
+            return dp[ind1][ind2] = Math.max(func(ind1,ind2-1,s1,s2,dp),func(ind1-1,ind2,s1,s2,dp));
+        }
+    }
+}
