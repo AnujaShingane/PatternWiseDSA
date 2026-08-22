abbe8aa4967fbaa4e2469f06123b091cff55831e
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-08-22 13:58:17 +0530

    Update Cycle Detection Directed Graph (DFS + Rec Stack).java

--- Graph/Cycle Detection Directed Graph (DFS + Rec Stack).java
+++ Graph/Cycle Detection Directed Graph (DFS + Rec Stack).java
@@ -1,31 +1,28 @@
-Use : vis + pathVis
-    1. dfs overall
-    2. mark vis pathVis of node true 
-    3. for each nei of node if not vis dfs if vis and pathVis return true
-    4. while backtracking mark pathVis as false while returning
+//main driver
+boolean[] vis = new boolean[n];
+boolean[] pathVis = new boolean[n];
 
-public static void main(){
-        List<List<Integer>> adj = adjList(prerequisites,numCourses); 
-        boolean[] vis = new boolean[n]; 
-        boolean[] pathvis = new boolean[n]; 
- 
-        for(int i = 0 ; i < n ; i++){ 
-            if(!vis[i]){ 
-                if(cycleDetection(i,vis,pathvis,adj))return false; 
-            } 
-        } 
- 
-        return true;
+for (int i = 0; i < n; i++) {
+    if (!vis[i]) {
+        if (cycleDetection(i, vis, pathVis, adj)) {
+            return true; // cycle exists
+        }
+    }
 }
 
-public boolean dfsDirected(int node, boolean[] vis, boolean[] pathVis, List<List<Integer>> adj) {
+return false;
+
+//logic
+public boolean cycleDetection(int node, boolean[] vis, boolean[] pathVis, List<List<Integer>> adj) {
     vis[node] = true;
     pathVis[node] = true;
 
     for (int nei : adj.get(node)) {
         if (!vis[nei]) {
-            if (dfsDirected(nei, vis, pathVis, adj)) return true;
-        } else if (vis[nei] && pathVis[nei]) {
+            if (cycleDetection(nei, vis, pathVis, adj)) {
+                return true;
+            }
+        } else if (pathVis[nei]) {
             return true;
         }
     }
